4451ef03829f4ea3e7ce00e624b17034e2c493a4
Matthew Smith <m@lattejed.com>
2025-06-11 16:11:45 +0700

    feat: finish content

--- src/js/main.js
+++ src/js/main.js
@@ -73,7 +73,7 @@ const setTypingEffectObserver = (ids) => {
     {
       root: null,
       rootMargin: "0px",
-      threshold: 0.25,
+      threshold: 0.1,
     }
   );
   observer.observe(elements[0].element);
